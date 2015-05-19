88de68ad2c945267147e73b743243e470974023b
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-19 03:03:46 +0200

    Update curried.scm

--- curried.scm
+++ curried.scm
@@ -18,8 +18,22 @@
   (lambda (kons-cell) (kons-cell (lambda (a b) b))))
 
 
-
 (define binary-add
   (curry 2 (lambda (a b) (+ a b))))
   
 (define add5 (binary-add 5))
+
+(define (applicable min max proc)
+  (define (loop proc args)
+    (lambda xs 
+      (set! args (append args xs))
+      (let ((largs (length args)))
+        (if (and (>= largs min) 
+                 (or (eq? max 'infinity)
+                     (<= largs max)))
+            (apply proc args)
+            (loop proc args)))))
+  (loop proc '()))
+
+
+
