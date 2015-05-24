e2cf7915f57598150fd0edcb99e0d00f9a07fe54
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-24 11:12:15 +0200

    Update and rename belangExamples.scm to examples.scm

---
+++ lang/v1/examples.scm
@@ -0,0 +1,34 @@
+
+(define foo
+  (fun (x -> 
+    ((fun -> (display x)
+             (newline)))))))
+
+(define bar
+  (fun (x ->
+    ((fun (x -> (display x)
+                (newline))) (+ x x)))))
+
+(define baz
+  (fun (0 -> (display "the end"))
+       (n -> (display n) 
+             (newline) 
+             (baz (- n 1)))))
+
+
+((fun (0 -> (display "let's GO!"))
+      (n -> (display n) 
+            (newline) 
+            (loop (- n 1)))) 3)
+
+
+(define infiniteA
+  (fun -> (display "infinite...")
+          (loop)))
+
+(define infiniteB
+  (fun -> (display "infinite...")
+          (infiniteB)))
+
+
+
