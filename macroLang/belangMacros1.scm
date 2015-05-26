42300b7f79bb7edfea9e0e987e87fb95fea8d519
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-26 13:58:24 +0200

    Update belangMacros1.scm

--- macroLang/belangMacros1.scm
+++ macroLang/belangMacros1.scm
@@ -1,1 +1,30 @@
-;;
+
+(define-syntax fun*
+  (syntax-rules ()
+    ((_ branch ...)
+     (let* ((branches (map parse-branch `(branch ...)))
+	    (arity (length (car (car branches)))))
+       (curry-fun arity branches)))))
+
+(define (curry-fun arity unevaluated)
+  (define (loop passed arity)
+    (lambda args
+      (let ((arity (- arity (length args)))
+            (args (append passed args)))
+        (if (<= arity 0) (evaluate-fun-branches unevaluated args) (loop args arity)))))
+  (loop '() arity))
+
+(define (evaluate-fun-branches branches args)
+  (if (null? branches)
+    'error
+    (begin
+      (display "ARGS...\n")
+      (display args)
+      (newline)
+      (display "BRANCHES...\n")
+      (for-each (lambda (b)
+        (display b)
+        (newline)) 
+          branches)
+      (match branches args))))
+
